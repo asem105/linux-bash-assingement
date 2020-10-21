be8c1383233ce87c0a8983707006da2f181a0f7b
zombies <zombiesboom0@gmail.com>
2020-10-21 19:53:03 +0530

    Ques 2 Added

---
+++ Final velocity/Velocity.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+echo "Write a BASH shell script to calculate final velocity (v) of an object, where an initial velocity (u), acceleration (a) and time_duration (t) is given as an input. The formula that will be used is v = u + a*t."
+
+echo "Enter velocity (u)"
+
+read u
+
+echo "acceleration (a)"
+
+read a
+
+echo "time_duration (t)"
+
+read t
+
+vv=`expr $a \* $t`
+
+v=`expr $u + $vv`
+
+echo "Final velocity = " $v
+
+read b
+
+fi
